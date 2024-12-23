41e57d77f9ccbf80f47de85393b55f544739f6a9
Giarno Divino <giar@Giarnos-MacBook-Air.local>
2024-12-23 18:33:12 +0800

    object-detection

--- video.py
+++ video.py
@@ -1,5 +1,8 @@
 import cv2
 
+from ultralytics import YOLO
+import supervision as sv
+
 stream = cv2.VideoCapture(0)
 
 if not stream.isOpened():
@@ -14,6 +17,13 @@ output = cv2.VideoWriter("stream.mp4",
                 cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
                 fps=fps, frameSize=(width, height))
 
+model = YOLO("yolov8n.pt")
+
+box_annotator = sv.BoxAnnotator(
+    thickness=2,
+)
+label_annotator = sv.LabelAnnotator()
+
 while(True):
     ret, frame = stream.read()
     if not ret:
@@ -22,6 +32,14 @@ while(True):
 
     frame = cv2.resize(frame, (width, height))
     output.write(frame)
+
+    result = model(frame)[0]
+    detections = sv.Detections.from_ultralytics(result)
+
+    frame = box_annotator.annotate(scene=frame, detections=detections)
+    frame = label_annotator.annotate(scene=frame, detections=detections)
+
+
     cv2.imshow("Webcam", frame)
     if cv2.waitKey(1) == ord('q'):
         break
